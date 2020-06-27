da63f5f064b1825654c68f337596e0d43e6e02d6
scottliu <liuyannan0721@hotmail.com>
2020-06-27 14:01:07 -0700

    update database and create superuser

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField
 # Create your models here.
+class Post(models.Model): 
+    title = models.TextField(blank = True, null = True)
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts', #image 都储存在post里面
+        format = 'JPEG',
+        options = {'quality':100},
+        blank = True,
+        null = True,
+    )
