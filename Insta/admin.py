6452043f9332f56dcd52f2dfe4b921b50ab3213c
scottliu <liuyannan0721@hotmail.com>
2020-07-02 19:26:34 -0700

    current version

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from Insta.models import Post, InstaUser  
+from Insta.models import Post, InstaUser, Like  
 # Register your models here.
 
 admin.site.register(Post) 
 admin.site.register(InstaUser) #往admin这个网站上面register这个自定义的user
+admin.site.register(Like)
--- Insta/models.py
+++ Insta/models.py
@@ -3,18 +3,6 @@ from imagekit.models import ProcessedImageField
 from django.urls import reverse 
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class Post(models.Model): 
-    title = models.TextField(blank = True, null = True)
-    image = ProcessedImageField(
-        upload_to = 'static/images/posts', #image 都储存在post里面
-        format = 'JPEG',
-        options = {'quality':100},
-        blank = True,
-        null = True,
-    )
-    #if someone create a post, it will redirect to get_absolute_url
-    def get_absolute_url(self):
-        return reverse("post_detail", args = [str(self.id)])
 
 class InstaUser(AbstractUser): #在继承Django中自带的AbstractUser后再自己定义一个profile picture
     profile_pic = ProcessedImageField(
@@ -24,3 +12,63 @@ class InstaUser(AbstractUser): #在继承Django中自带的AbstractUser后再自
         blank = True,
         null = True,
     )
+
+class Post(models.Model): 
+    author = models.ForeignKey( #Post也应该是一个foreign key，因为其应是指向某一个发这个post的用户
+        InstaUser,
+        on_delete = models.CASCADE,
+        related_name = 'my_posts' #当我处于当前用户时，我可以通过my_posts来找到我发过的posts.
+    )
+    title = models.TextField(blank = True, null = True)
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts', #image 都储存在post里面
+        format = 'JPEG',
+        options = {'quality':100},
+        blank = True,
+        null = True,
+    )
+    def get_like_count(self):
+        return self.likes.count()
+        
+    #if someone create a post, it will redirect to get_absolute_url
+    def get_absolute_url(self):
+        return reverse("post_detail", args = [str(self.id)])
+
+
+
+class Like(models.Model):
+    #这个like model应该告诉我们哪一个用户喜欢哪一个post
+    #因此这个model应该是一个关系型model,其应该联系起Post和InstaUser之间的关系
+    #Foreignkey 表示这个post并不是哪一个具体的类型，是一个外键，指向post这个model的一个key.
+    #并且当post被删除时，Like这个关系也会被删除： on_delete = models.CASCADE
+    #
+    post = models.ForeignKey(
+        Post,
+        on_delete = models.CASCADE,
+        related_name='likes')
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete = models.CASCADE,
+        related_name = 'likes'
+    )
+    #这个like model里面定义了两个fields: post 和 user， 且他们都是外键，他们指向别的model: Post, InstaUser
+    #而这个 “likes” 可以帮助在当前外键下找到作用于自己的Like这个关系，比如在某一篇Post下，通过'likes'便可以找到所有对
+    #这篇Post点赞的user. 在User上反之便可找到所有自己点过的likes.
+
+    class Meta:
+        unique_together = ("post", "user")
+        #一个user只可以给一篇post点一个赞
+        #即同样的user和post只能出现一次
+    
+    def __str__(self):
+            return 'Like: ' + self.user.username + ' likes ' + self.post.title
+
+
+# class Comment(models.Model):
+#     post = models.ForeignKey()
+#     user = models.ForeignKey()
+#     comment = models.CharField(max_length = 100)
+#     posted_on = models.DateTimeField(auto_now_add = True, editable = False)
+
+#     def __str__(self):
+#         return self.comment
